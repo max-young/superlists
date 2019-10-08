93e45aadf99c7459917200a3ea1a1c64359f9659
yangle <yangle@luojilab.com>
2019-10-08 18:38:01 +0800

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -29,15 +29,18 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她在一个文本框中输入了"Buy peacock feathers"(购买孔雀羽毛)
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
-        inputbox.sned_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # 她按回车键后, 页面更新了
         # 待办事项中显示了"1: Buy peacock feathers"
-        inputbox.sned_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_elements_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
         # 页面中又显示了一个文本框, 可以输入其他的待办事项
         # 她输入了"Use peacock feathers to make a fly"(使用孔雀羽毛做假蝇)
