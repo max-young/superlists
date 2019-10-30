af51d2a15bee769646dcc2122c63005b6c74ba33
yangle <yangle@luojilab.com>
2019-10-30 12:04:21 +0800

    rename all item input ids and names. still broken

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -9,10 +9,10 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
 
         # 她看到输入框完美地居中显示
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 270, delta=5)
 
         # 她新建了一个清单, 看到输入框仍完美地居中显示
         inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 270, delta=5)
